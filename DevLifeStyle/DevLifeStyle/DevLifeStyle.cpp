89d02a61b1708079bee808a397299f9a356fde47
Eduardo Bortolotte <bortolotte@gmail.com>
2022-11-10 22:50:41 -0300

    Funções, parâmetros, tipo de retorno, depuração (step in/out/over)
    Signed-off-by: Eduardo Bortolotte <bortolotte@gmail.com>

--- DevLifeStyle/DevLifeStyle/DevLifeStyle.cpp
+++ DevLifeStyle/DevLifeStyle/DevLifeStyle.cpp
@@ -8,6 +8,62 @@ using namespace std;
  linhas
 */
 
+/*
+* Esta função recebe a opção digitada pelo usuário
+*/
+int recebeOpcao()
+{
+    int option;
+    printf("Escolha a operacao (1 - soma, 2 - subtracao, 3 - multiplicacao, 4 - divisao, 5 - sair): ");
+    scanf_s("%d", &option);
+    return option;
+}
+
+int recebePrimeiroNumero()
+{
+    int numero;
+    cout << "Digite o primeiro numero: ";
+    cin >> numero;
+    return numero;
+}
+
+int recebeSegundoNumero()
+{
+    int numero;
+    cout << "Digite o segundo numero: ";
+    cin >> numero;
+    return numero;
+}
+
+int calcula(int operandoA, int operandoB, int operador)
+{
+    int resultado;
+    switch (operador)
+    {
+    case 1:
+        resultado = operandoA + operandoB;
+        break;
+    case 2:
+        resultado = operandoA - operandoB;
+        break;
+    case 3:
+        resultado = operandoA * operandoB;
+        break;
+    case 4:
+        resultado = operandoA / operandoB;
+        break;
+    default:
+        resultado = 0;
+    }
+    return resultado;
+}
+
+void imprimeResultado(int resultado)
+{
+    cout << "O resultado da operacao eh: " << resultado << "\n";
+    // return; desnecessário
+}
+
 int main() // função principal
 {
     // entry point
@@ -33,43 +89,17 @@ int main() // função principal
     // void
     //void v1;  // inválido. não declaramos variáveis void
 #pragma endregion
-    /*int contagem = 0;
-    while (contagem < 5)
-    {
-        printf("Iteracao %d\n", contagem);
-        contagem = contagem + 1;
-    }
-    printf("Termino do while\n");*/
-
-    for (int contagem = 0; contagem < 15; contagem++)
-    {
-        printf("Inicio da iteracao %d\n", contagem);
-
-        if (contagem == 2)
-            continue;
-
-        if (contagem == 5)
-            break;  // sai do loop, mesmo que a condição do cabeçalho ainda seja verdadeira
-
-        printf("Final da iteracao %d\n", contagem);
-    }
-    printf("Termino do for\n");
 
     cout << "Programa de operacoes aritmeticas entre 2 numeros.\n\n";
 
     while (true)
     {
-        printf("Escolha a operacao (1 - soma, 2 - subtracao, 3 - multiplicacao, 4 - divisao, 5 - sair): ");
-        scanf_s("%d", &opcao);
+        // aquisição
+        opcao = recebeOpcao();
         if (opcao == 5)
             break;
-
-        cout << "Digite o primeiro numero: ";
-        cin >> numero1;
-
-        cout << "Digite o segundo numero: ";
-        cin >> numero2;
-
+        numero1 = recebePrimeiroNumero();
+        numero2 = recebeSegundoNumero();
         if (numero1 > 0 && numero2 > 0) // operador lógico 'E'
             cout << "Ambos positivos" << "\n";
         else if (numero1 < 0 || numero2 < 0) // operador lógico 'OU'
@@ -88,24 +118,7 @@ int main() // função principal
         else
             resultado = 0;*/
 #pragma endregion 
-
-        switch (opcao)
-        {
-        case 1:
-            resultado = numero1 + numero2;
-            break;
-        case 2:
-            resultado = numero1 - numero2;
-            break;
-        case 3:
-            resultado = numero1 * numero2;
-            break;
-        case 4:
-            resultado = numero1 / numero2;
-            break;
-        default:
-            resultado = 0;
-        }
+        resultado = calcula(numero1, numero2, opcao);
 
         // operadores lógicos: ==, !=, <, >, <=, >=
 
@@ -116,6 +129,6 @@ int main() // função principal
         }   // término de um bloco de instruções ou escopo
 
         // operadores aritméticos + - / *
-        cout << "O resultado da operacao eh: " << resultado << "\n";
+        imprimeResultado(resultado);
     }
 }
